fdd82b170d4f39e87fbbba218c1b90fe4183824e
Tyler Sahagun <tylersahagun@gmail.com>
2025-01-30 16:20:53 -0700

    Preparing for GitHub initialization

---
+++ LeetCodeExtension/LeetCodeService.swift
@@ -0,0 +1,8 @@
+//
+//  LeetCodeService.swift
+//  LeetCodeExtension
+//
+//  Created by Tyler Sahagun on 1/30/25.
+//
+
+import Foundation
---
+++ LeetCodeExtension/SourceEditorCommand.swift
@@ -0,0 +1,19 @@
+//
+//  SourceEditorCommand.swift
+//  LeetCodeExtension
+//
+//  Created by Tyler Sahagun on 1/30/25.
+//
+
+import Foundation
+import XcodeKit
+
+class SourceEditorCommand: NSObject, XCSourceEditorCommand {
+    
+    func perform(with invocation: XCSourceEditorCommandInvocation, completionHandler: @escaping (Error?) -> Void ) -> Void {
+        // Implement your command here, invoking the completion handler when done. Pass it nil on success, and an NSError on failure.
+        
+        completionHandler(nil)
+    }
+    
+}
